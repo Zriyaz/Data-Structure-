d4ca370325fffbe6a25d239524f3828eabd967a4
Md Riyaz Kazmi <mdriyazuddine17@gmail.com>
2020-04-30 06:36:29 +0000

    return 0;

--- QueueStack.cpp
+++ QueueStack.cpp
@@ -4,7 +4,7 @@ using namespace std;
 class Queue
 {
   private:
-  stack<int>s1,s2;
+  stack<int>s1;
   //Method 2 (By making deQueue operation costly)
   public:
   void enqueue(int x)
@@ -13,28 +13,33 @@ class Queue
   }
   int dequeue()
   {
-    if(s1.empty() && s2.empty()){
-    cout<<"Queue is full";
-    exit(0);
-    }
-    if(s2.empty()){
-    while(!s1.empty()){
-      s2.push(s1.top());
-      s1.pop();
-    }
-    }
-    int data=s2.top();
-    s2.pop();
-    return data;
+     if(s1.empty())
+     {
+       cout<<"Queue is empty"<<endl;
+       exit(0);
+     }
+     int data=s1.top();
+     s1.pop();
+
+     if(s1.empty())
+     return data;
+
+     int temp=dequeue();
+     s1.push(data);
+     return temp;
   }
 };
+
 int main() {
    Queue q; 
     q.enqueue(1); 
     q.enqueue(2); 
     q.enqueue(3); 
+  
     cout << q.dequeue() << '\n'; 
     cout << q.dequeue() << '\n'; 
     cout << q.dequeue() << '\n'; 
+  
     return 0; 
+  
 }
